726ca62dc81676780969925f895c0a0e0a8b0e65
MiDDiz <xxmidone30xx@gmail.com>
2020-01-09 01:28:55 +0100

    Fixed sum bad practices, changed while 1 loop to a recursive function

--- bank.cpp
+++ bank.cpp
@@ -1,14 +1,23 @@
+/*  
+    Queda cutre decirlo con un pequeño programa pero es buena practica xd:
+    License CC-BY-SA for www.github.com/MiDDiz or Joan Carlos Naftanaila
+    Last C++ excercise from my CS assignature.
+    You can watch some others repos on https://www.github.com/MiDDiz or in https://middiz.github.io/main
+*/
 #include <iostream>
 using namespace std;
 double balance = 2000; //Balance es global para solucionar quebraderos de cabeza y tener que pasarlo de una funcion a otra :)
-
-//Esta funcion es simplemente 'decorativa'\
-  Solo se usa para que el programa se pause y no escupa lineas de texto sin que se entere el usuario.
+void opciones();
+//La mayoria de esta funcion es simplemente 'decorativa'\
+  Se usa para que el programa se pause y no escupa lineas de texto sin que se entere el usuario.\
+  Esto tambien genera un bug ya que si podes un caracter que pueda valer en las opciones y acto despues otro igual lo tomara como\
+  si fueran caracteres introducidos individualmente -> La estetica pesa mas que este bug asi que se queda :D
 void volverAlMenu(){
     char foo;
     cout << "Pulsa enter para volver al menu..." << endl;
     foo = getchar();
     cin.ignore();
+    opciones();
 }
 //Bastante autoexplicativo, aqui vemos la utilidad de volverAlMenu en accion
 void verSaldo(){
@@ -26,7 +35,7 @@ bool comprobarDinero(int dineroASacar, double balanceTotal){
 }
 //Operaciones aritmeticas. Si el bloque if entra, el else no asi que no hay necesidad de returnear.
 void billetes(int cantidad){
-    if(cantidad % 20 != 0) cout << "Este cajero solo dispone de billetes de 20 y de 50 por lo que no podemos continuar con la transaccion.\nDisculpe las molestias..." << endl;
+    if(cantidad % 20 != 0 && cantidad % 50 !=0 ) cout << "Este cajero solo dispone de billetes de 20 y de 50 por lo que no podemos continuar con la transaccion.\nDisculpe las molestias..." << endl;
     else {
         int billetesc = 0;
         int billetesv = 0;
@@ -50,7 +59,8 @@ void billetes(int cantidad){
 void sacarDinero(){
     int cantidadASacar;
     cout << "Introduce la cantidad de dinero que quieres sacar: " <<endl;
-    cin >> cantidadASacar;
+    //Hay un bug aqui. Si introduces algo que no sea un numero el programa se vuelve loco en vez de caer asi que no puedo pillarlo con un try-catch :(
+    cin >> cantidadASacar; 
     //Auto explicativo, si devuelve la funcion true es que el dinero a sacar es mayor.
     if (comprobarDinero(cantidadASacar, balance)) {
         cout << "La cantidad de dinero a sacar supera su saldo actual" << endl;
@@ -79,23 +89,36 @@ void sacarDinero(){
     }
 
 }
+void opciones(){
+    char option;
+    cout << "(v) - Ver saldo" << endl;
+    cout << "(d) - Sacar dinero" << endl;
+    cout << "(s) - Salir" << endl;
+    cin >> option;
+    switch (option)
+    {
+    case 'v':
+        verSaldo();
+        break;
+    case 'd':
+        sacarDinero();
+        break;
+    case 's':
+        cout << "Vale, adios!" << endl;
+        return;
+    default:
+        cout << "No he entendido eso, podrias repetirlo?" << endl;
+        opciones(); //Uso de recursion para evitar loops infinitos innecesarios.
+        break;
+    }
+ 
+}
 
 int main(){
-    char option;
-    //Todo el programa se encuentra en un bucle infinito en el que solo se puede salir por el return 0 cuando option = s
-    while (1){
+    //El bucle del programa lo maneja la recursion de opciones(). Al salir con 's' se cierran todos los switch abiertos y entra en el return 0 del main.
+    
         cout << "Bienvenido a RoBankia" << endl; //Lo pillas? Roba-bankia jaja... un poco de humor no hace mal.
         cout << "Que es lo que quieres hacer hoy?\n" << endl;
-        cout << "(v) - Ver saldo" << endl;
-        cout << "(d) - Sacar dinero" << endl;
-        cout << "(s) - Salir" << endl;
-        cin >> option;
-        if (option == 'v') verSaldo();
-        else if (option == 'd') sacarDinero();
-        else if (option == 's'){
-            cout << "Vale, adios!" << endl;
-            return 0;
-        } else cout << "No he entendido eso, podrias repetirlo?" << endl;
-    }
-    
+        opciones();
+        return 0;
 }
